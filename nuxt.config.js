cb22f29b99ff1e30392afe54b5e28cb81f112b29
anhtoan260497 <anhtoan260497@gmail.com>
2022-02-27 10:50:47 +0700

    fix error

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,29 +1,33 @@
+import colors from 'vuetify/es5/util/colors'
+
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'CellphoneS - Điện thoại, laptop, tablet, phụ kiện chính hãng ',
+    title: 'CellphoneS - Điện thoại, laptop, tablet, phụ kiện chính hãng',
     htmlAttrs: {
-      lang: 'en',
+      lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
+      { name: 'format-detection', content: 'telephone=no' }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    // '~assets/css/main.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -34,29 +38,14 @@ export default {
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/stylelint
     '@nuxtjs/stylelint-module',
+    // https://go.nuxtjs.dev/vuetify
     '@nuxtjs/vuetify',
-    '@nuxtjs/style-resources',
-
-    // With options
-    [
-      '@nuxtjs/vuetify',
-      {
-        /* module options */
-      },
-    ],
   ],
-  styleResources: {
-    // scss: [
-    //   '~assets/scss/mixins.scss',
-    //   '~assets/scss/variables.scss'
-    // ]
-  },
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
-    '@nuxtjs/vuetify'
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
@@ -64,16 +53,29 @@ export default {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/',
   },
+
+  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    // customVariables: ["~/assets/variables.scss"],
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
-  // nuxt.config.js
-
-    server: {
-      port: 8080 // default: 3000
-    }
-
+  build: {
+  },
+  server: {
+    port: 8000 // default: 3000
+  }
 }
---
+++ vue.config.js
@@ -0,0 +1,17 @@
+
+const { VuetifyLoaderPlugin } = require('vuetify-loader')
+
+exports.plugins.push(
+  new VuetifyLoaderPlugin()
+)
+module.exports = {
+    chainWebpack: config => {
+      config.plugin('VuetifyLoaderPlugin').tap(args => [{
+        match (originalTag, { kebabTag, camelTag, path, component }) {
+          if (kebabTag.startsWith('core-')) {
+            return [camelTag, `import ${camelTag} from '@/components/core/${camelTag.substring(4)}.vue'`]
+          }
+        }
+      }])
+    }
+  }
